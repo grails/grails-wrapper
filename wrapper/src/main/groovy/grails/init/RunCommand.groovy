162aa65b8f81a4d8c3c10ba15f13f86fd46df75e
James Fredley <jamesfredley@users.noreply.github.com>
2025-04-18 17:57:46 -0400

    Use Apache Grails Maven Coordinates

--- wrapper/src/main/groovy/grails/init/RunCommand.groovy
+++ wrapper/src/main/groovy/grails/init/RunCommand.groovy
@@ -41,11 +41,11 @@ class RunCommand {
 
         MavenResolverGrapeEngine grapeEngine = MavenResolverGrapeEngineFactory.create(groovyClassLoader, repositoryConfigurations, new DependencyResolutionContext(), false)
         try {
-            grapeEngine.grab([:], [group: "org.grails", module: "grails-shell", version: grailsVersion])
+            grapeEngine.grab([:], [group: "org.apache.grails", module: "grails-shell-cli", version: grailsVersion])
         }
         catch(dependencyResolutionException){
             // Try grails shell version from gradle.properties or default
-            grapeEngine.grab([:], [group: "org.grails", module: "grails-shell", version: grailsShellVersion])
+            grapeEngine.grab([:], [group: "org.apache.grails", module: "grails-shell-cli", version: grailsShellVersion])
         }
 
         ClassLoader previousClassLoader = Thread.currentThread().contextClassLoader
